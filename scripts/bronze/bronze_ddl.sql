0ef2d7ebf437e282bfb2b251ec3a1b78bd3f3efd
Gomem <mkby6jgfcv@privaterelay.appleid.com>
2026-05-26 09:50:31 +1000

    Update bronze_ddl.sql
    dwh date not needed in bronze

--- scripts/bronze/bronze_ddl.sql
+++ scripts/bronze/bronze_ddl.sql
@@ -8,8 +8,7 @@ CREATE TABLE crm_cust_info (
 	cst_lastname VARCHAR(50) NULL,
 	cst_marital_status VARCHAR(50) NULL,
 	cst_gndr VARCHAR(50) NULL,
-	cst_create_date DATE NULL,
-    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	cst_create_date DATE NULL
 );
 DROP TABLE IF EXISTS crm_prd_info; 
 CREATE TABLE crm_prd_info (
@@ -19,8 +18,7 @@ CREATE TABLE crm_prd_info (
 	prd_cost DECIMAL(10,2) NULL,
     prd_line VARCHAR(50) NULL,
 	prd_start_dt DATE NULL,
-    prd_end_dt DATE NULL,
-    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+    prd_end_dt DATE NULL
 );
 DROP TABLE IF EXISTS crm_sales_details; 
 CREATE TABLE crm_sales_details (
@@ -32,27 +30,23 @@ CREATE TABLE crm_sales_details (
 	sls_due_dt DATE NULL,
     sls_sales DECIMAL(10,2) NULL,
     sls_quantity INT,
-    sls_price DECIMAL(10,2) NULL,
-    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+    sls_price DECIMAL(10,2) NULL
 );
 DROP TABLE IF EXISTS erp_cust_az12; 
 CREATE TABLE erp_cust_az12 (
 	cid VARCHAR(50) NULL,
 	bdate DATE NULL,
-	gen VARCHAR(50) NULL,
-    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	gen VARCHAR(50) NULL
 );
 DROP TABLE IF EXISTS erp_loc_a101; 
 CREATE TABLE erp_loc_a101 (
 	cid VARCHAR(50) NULL,
-	cntry VARCHAR(50) NULL,
-    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	cntry VARCHAR(50) NULL
 );
 DROP TABLE IF EXISTS erp_px_cat_g1v2; 
 CREATE TABLE erp_px_cat_g1v2 (
 	id VARCHAR(50) NULL,
     cat VARCHAR(50) NULL,
 	subcat VARCHAR(50) NULL,
-    maintenance VARCHAR(50) NULL,
-    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+    maintenance VARCHAR(50) NULL
 );
